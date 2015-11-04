c17bb194082002942fc6cf2bc04dfbe2d46049a1
pekj <pekj@umich.edu>
2015-11-04 17:50:01 -0500

    Created Enemy prefab, Began implementing Enemy detection of Player

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+	NavMeshAgent navMesh;
+	PlayerController playerScript;
+
+	// Use this for initialization
+	void Start () {
+		navMesh = GetComponent<NavMeshAgent> ();
+		playerScript = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ();
+
+		navMesh.SetDestination(transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag != "Player")
+			return;
+
+		/*if (playerScript.isHidden()) {
+			set destination to player
+		}*/
+	}
+}
