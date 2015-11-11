fb773f6e6c43cfa3c650c5893cc4ab108888f3a7
Karl R. Henderson <karlrhen@umich.edu>
2015-11-11 13:05:53 -0500

    PointLight Prefab is created and Point/Spot Light Shadows also implemented

---
+++ Assets/Scripts/SpotLightShadow.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpotLightShadow : MonoBehaviour {
+
+	public GameObject player;
+	PlayerController behavior;
+	float range;
+	bool playerIsInSpotLight = false;
+
+	// Use this for initialization
+	void Start () {
+		range = gameObject.GetComponent<Light>().range;
+		behavior = player.GetComponent<PlayerController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+ 		RaycastHit hit; 
+		var rayDirection = player.transform.position - range*transform.position;
+		Debug.DrawRay(transform.position, rayDirection * 5f, Color.blue);
+		if (Physics.Raycast (transform.position, rayDirection, out hit)) 
+		{	
+ 			if (hit.transform.tag == "Player") 
+			{
+				if (!playerIsInSpotLight) 
+				{
+					behavior.numLights++;
+				}
+				playerIsInSpotLight = true; 
+				print("IN THE LIGHT");
+
+			} 
+			else 
+			{
+				if (playerIsInSpotLight) 
+				{
+					behavior.numLights--;
+				}
+				playerIsInSpotLight = false; 
+				print ("OUT OF THE LIGHT");
+			}
+		} 
+		else 
+		{
+			if (playerIsInSpotLight)
+			{
+				behavior.numLights--;
+			}
+			playerIsInSpotLight = false; 
+			print ("OUT OF THE LIGHT");
+		}
+	}
+}
