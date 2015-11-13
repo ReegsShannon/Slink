b8f933028f276883ca8570b76e85c9cd322d2844
reegs <reegs@umich.edu>
2015-11-12 23:39:27 -0500

    merge

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPoint : MonoBehaviour {
+	
+	static GameObject player;
+	static Vector3 respawnSpot;
+
+	void Awake(){
+		player = GameObject.FindGameObjectWithTag ("Player");
+		if (player != null) {
+			respawnSpot = player.transform.position;
+		} else {
+			respawnSpot = Vector3.zero;
+		}
+	}
+
+	/*static CheckPoint(){
+		player = GameObject.FindGameObjectWithTag ("Player");
+		if (player != null) {
+			respawnSpot = player.transform.position;
+		} else {
+			respawnSpot = Vector3.zero;
+		}
+	}
+	*/
+	
+	public static void respawn(){
+		player.transform.position = respawnSpot;
+	}
+	
+	void OnTriggerEnter(Collider coll){
+		if(player.GetInstanceID() == coll.gameObject.GetInstanceID())
+			respawnSpot = player.transform.position;
+	}
+	
+	void Update(){
+		print (respawnSpot);
+	}
+}
