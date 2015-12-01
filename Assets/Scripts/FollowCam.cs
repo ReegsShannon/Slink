133a36616c3ca317dbce54f124d0baaf048d152c
reegs <reegs@umich.edu>
2015-12-01 15:03:01 -0500

    wall camera fix

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -9,10 +9,8 @@ public class FollowCam : MonoBehaviour {
 	public float			distance = 7, height = 2;
 	public float			u = 0.1f;
 	public PlayerController behavior;
-	public float minDistance = 1;
 
-	float curDistance;
-	bool collide;
+	public LayerMask mask;
 
 	// Use this for initialization
 	void Start () {
@@ -20,34 +18,22 @@ public class FollowCam : MonoBehaviour {
 		camTarget = player.transform;
 		poi = GameObject.Find("CamTarget").transform;
 		behavior = player.GetComponent<PlayerController> ();
-		curDistance = distance;
-		collide = false;
 	}
 	
 	void FixedUpdate () {
-		/*if (collide) {
-			curDistance = Mathf.Lerp (curDistance, minDistance, u);
-		} else {
-			curDistance = Mathf.Lerp (curDistance, distance, u);
-		}*/
 		Vector3 pos = poi.position;
-		pos -= poi.forward * curDistance;
+		pos -= poi.forward * distance;
 		pos += poi.up * height;
-		
-		Vector3 pos2 = (1-u)*transform.position + u*pos;
-		transform.position = pos2;
-		
-		//transform.LookAt(camTarget);
-		transform.LookAt (poi);
-		collide = false;
-	}
 
-	/*void OnCollisionStay(Collision collision){
-		print ("collide");
-		collide = true;
+		RaycastHit hit;
+		Vector3 targetPosition = pos;
+		Vector3 playerPos = player.transform.position + Vector3.up * .1f;
+		if (Physics.Raycast (pos, playerPos - pos, out hit, (playerPos - pos).magnitude, mask.value)) {
+			targetPosition = (hit.point - playerPos) * .8f + playerPos;
+		}
+
+		pos = targetPosition;
+		transform.position = Vector3.Lerp (transform.position, pos, u);
+		transform.LookAt (poi);
 	}
-	void OnTriggerStay(Collider collision){
-		print (collision.gameObject.name);
-		if(collision.gameObject.tag != "Checkpoint") collide = true;
-	}*/
 }
