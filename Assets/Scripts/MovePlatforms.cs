8baac285aa1cbad9fdb922122d659a9da2b60dbc
Karl R. Henderson <karlrhen@umich.edu>
2015-12-14 14:02:55 -0500

    last level change

---
+++ Assets/Scripts/MovePlatforms.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class MovePlatforms : MonoBehaviour {
+
+	public float timer = 0;
+	public bool zMovement = false;
+	public float speed;
+	public float oscillationSeconds;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		timer += Time.deltaTime;
+		if (timer < oscillationSeconds) {
+
+			if(zMovement){
+				this.transform.Translate(Vector3.forward*Time.deltaTime*speed);
+			}
+			else{
+				this.transform.Translate(Vector3.back*Time.deltaTime*speed);
+			}
+		} 
+		else 
+		{
+			timer = 0;
+			if(zMovement == true){
+				zMovement = false;
+			}
+			else{
+				zMovement = true;
+			}
+		}
+	}
+
+
+}
